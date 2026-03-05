5039cd15e995de9e35ae1cc0e24bdb0b21b596a1
adindayuriafifah14 <adindaafifah14@gmail.com>
2026-03-05 10:48:50 +0700

    Menambahkan elemen

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -4,3 +4,15 @@ using namespace std;
  
 int arr[20]; //Deklarasi variable global array a dengan ukuran 20
 int n; //Deklarasi variable global n untuk menyimpan banyaknya elemen
+
+void input() {          //prosedur untuk input
+    while (true) {
+        cout << "Masukkan banyaknya elemen pada array : "; 
+        cin >> n; 
+        if (n<=20)
+            break;
+        else {
+            cout << "\nArray dapat mempunyai maksimal 20 elemen.\n";
+        }
+    }
+}
